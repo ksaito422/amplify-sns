2854453999017017086c97396854fa599ebe6b7f
saitooooooo <kitsit.39@gmail.com>
2022-12-20 09:20:19 +0900

    feat: jsxに変更してcognitoの導入

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+const App = () => {
+	const [authState, setAuthState] = useState();
+	const [user, setUser] = useState();
+
+	useEffect(() => {
+		return onAuthUIStateChange((nextAuthState, authData) => {
+			setAuthState(nextAuthState);
+			setUser(authData);
+		});
+	}, []);
+
+	return authState === AuthState.SignedIn && user ? (
+		<div className="App">
+			<div>Hello, {user.username}</div>
+			<AmplifySignOut />
+		</div>
+	) : (
+		<AmplifyAuthenticator>
+			<AmplifySignUp
+				slot="sign-up"
+				formFields={[
+					{ type: "username" },
+					{ type: "password" },
+					{ type: "email" }
+				]}
+			/>
+		</AmplifyAuthenticator>
+	);
+}
+
+export default App;
